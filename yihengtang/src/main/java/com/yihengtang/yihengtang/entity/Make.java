4b70f15d4e0e5fcb52c8820804effe1ac58f940d
郭亚坤 <mrguoyakun@163.com>
2017-12-27 08:20:12 +0800

    Signed-off-by: 郭亚坤 <mrguoyakun@163.com>

---
+++ yihengtang/src/main/java/com/yihengtang/yihengtang/entity/Make.java
@@ -0,0 +1,28 @@
+package com.yihengtang.yihengtang.entity;
+
+public class Make {
+
+	private String name;
+	private String nick_name;
+	private int state;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNick_name() {
+		return nick_name;
+	}
+	public void setNick_name(String nick_name) {
+		this.nick_name = nick_name;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	
+	
+}
