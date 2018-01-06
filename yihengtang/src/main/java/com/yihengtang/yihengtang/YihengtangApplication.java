27a5ff86cd5e8e7cd8a3c9efce59fb10490fe653
郭亚坤 <mrguoyakun@163.com>
2018-01-06 17:16:03 +0800

    Signed-off-by: 郭亚坤 <mrguoyakun@163.com>

--- yihengtang/src/main/java/com/yihengtang/yihengtang/YihengtangApplication.java
+++ yihengtang/src/main/java/com/yihengtang/yihengtang/YihengtangApplication.java
@@ -3,7 +3,11 @@ package com.yihengtang.yihengtang;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 
 //MyBatis 支持
@@ -14,5 +18,6 @@ public class YihengtangApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(YihengtangApplication.class, args);
 	}
+
 	
 }
